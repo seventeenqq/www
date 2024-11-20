703b048c373210da69382e22d0203422e9e09e47
seventeenqq <ilyasurgut901@gmail.com>
2024-11-20 09:14:12 +0500

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,21 @@
+/*let nn = document.querySelector("#number");
+nn.addEventListener('keydown', function(event) {
+    let numb, eventik;
+    eventik = event.which;
+    numb = String.fromCharCode(a);
+    let regex = /[0-9\s\+\-]|\./;
+    if (!regex.test(numb))  {
+        eventik.returnValue = false;
+        if(eventik.preventDefault) eventik.preventDefault();
+    }
+});*/
+
+/* let input = document.querySelector("#name");
+input.addEventListener("keydown",(knopka) => {
+    let pressed_b = knopka.key;
+    let regex = ["+","-","1","2","3","4","5","6","7","8","9","0"];
+    let pr = regex.indexOf(pressed_b)
+    if (pr===-1){
+        pressed_b.value = pressed_b.value.replace(regex, '');
+    }
+}) */
